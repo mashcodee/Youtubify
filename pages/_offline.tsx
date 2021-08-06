7f09cdd39161c2f0bdbcd57d4bb1ddc45181d9b8
Mash Codee <codeemash@gmail.com>
2021-08-06 10:33:11 +0530

    offline page testing

---
+++ pages/_offline.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const Offline = (props: Props) => {
+  return (
+    <>
+      <div>You are offline</div>
+    </>
+  );
+};
+
+export default Offline;
